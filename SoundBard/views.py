aaaab3a2fd95ad4107e9b597be323f18a0ff6c6d
CoRExE <98487589+CoRExE@users.noreply.github.com>
2024-05-05 08:02:39 +0200

    Template

--- SoundBard/views.py
+++ SoundBard/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
 
 # Create your views here.
+
+def index(request):
+    context = {
+        "message": 'Plein de Sons Bientôt !'
+    }
+    template = loader.get_template('soundbard.index.html')
+    return HttpResponse(template.render(context, request))
